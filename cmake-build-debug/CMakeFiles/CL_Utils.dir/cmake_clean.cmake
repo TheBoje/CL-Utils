49e5c2e6f007630d3756b981b3a49c556ae63a2e
Vincent <vincent.commin@etu.univ-poitiers.fr>
2021-05-13 22:28:50 +0200

    [BUILD] - CMakeList

---
+++ cmake-build-debug/CMakeFiles/CL_Utils.dir/cmake_clean.cmake
@@ -0,0 +1,14 @@
+file(REMOVE_RECURSE
+  "CL_Utils.exe"
+  "CL_Utils.exe.manifest"
+  "CL_Utils.pdb"
+  "CMakeFiles/CL_Utils.dir/Graph/source/graph.c.obj"
+  "CMakeFiles/CL_Utils.dir/Graph/source/main.c.obj"
+  "CMakeFiles/CL_Utils.dir/Graph/source/node.c.obj"
+  "libCL_Utils.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/CL_Utils.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
